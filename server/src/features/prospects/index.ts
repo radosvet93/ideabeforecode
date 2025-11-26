79ac74aa27539273d4158a22ce5994a8cfa6ebfb
Radosvet Petrov <93radosvet@gmail.com>
2025-11-26 11:44:22 +0000

    get all prospects

--- server/src/features/prospects/index.ts
+++ server/src/features/prospects/index.ts
@@ -1,10 +1,11 @@
 import { Router, type Request, type Response } from "express";
-import { listProspects, type Prospect } from "./model";
+import { listProspects } from "./model";
+import type { Prospect } from "@shared/types";
 
 export const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
   const prospects: Prospect[] = await listProspects();
 
-  res.json(prospects[0]);
+  res.json(prospects);
 });
