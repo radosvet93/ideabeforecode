a4f65ae7aaeee300d6d331ae7dba06660644d6c7
Radosvet Petrov <93radosvet@gmail.com>
2025-12-21 18:14:30 +0000

    remove logs

--- client/src/services/lead/uploadLeads.ts
+++ client/src/services/lead/uploadLeads.ts
@@ -1,8 +1,6 @@
 import ky from 'ky';
 
 export const uploadLeads = async ({ file, projectId }: { file: File, projectId: string }) => {
-  console.log('file', file);
-  console.log('projectId', projectId);
   await ky.post(`/api/leads/upload-csv?projectId=${projectId}`, {
     body: file,
     headers: {
